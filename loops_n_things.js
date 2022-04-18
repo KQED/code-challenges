4b48162acb1ed0641d2f391ed7e9986521f1bca6
Jason Cater <jasondcater@gmail.com>
2022-04-18 13:55:24 -0700

    Adding some js challenges

---
+++ loops_n_things.js
@@ -0,0 +1,22 @@
+let thing1 = [
+  {
+    id: 123,
+    type: 'foo',
+    color: 0xffffff
+  },
+  {
+    id: '234',
+    type: 'bar',
+    color: 'red'
+  },
+  {
+    id: '123cfe',
+    type: 'baz',
+    color: 'blue'
+  }
+]
+
+// Loop array 'thing' one and output the fields in a table form
+console.log('id', 'type', 'color')
+
+
