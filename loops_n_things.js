b32754c7c27751ef8df61c59df868b0ed3d13975
Jason Cater <jasondcater@gmail.com>
2022-04-27 09:59:31 -0700

    Adjusting the challenge

--- loops_n_things.js
+++ loops_n_things.js
@@ -7,19 +7,18 @@ let thing1 = [
   },
   {
     id: '234',
-    type: 'bar',
+    'type': 'bar',
     color: 'red',
     count: '75'
   },
   {
     id: '123cfe',
     type: 'baz',
-    color: 'blue',
+    colour: 'blue',
     count: 123
   }
 ]
 
-console.log('id', 'type', 'color')
-
 // Loop array 'thing1' and output the fields in a table form
 // Sum the field 'count' and output the value
+// Add a row at the beginning of the output with the field names
